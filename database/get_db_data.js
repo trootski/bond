32f02c589edc3e7c422ff9fc3dcdddc4feaf1ccc
John Byrne <troot1@gmail.com>
2018-07-17 23:41:51 +0100

    feature: add database code

---
+++ database/get_db_data.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const request = require('request');
+const fs = require('fs');
+
+const filmTitles = require('./films.json');
+
+const apiKey = '78ab6537';
+
+const getMovieDetails = (title) => new Promise(resolve => {
+    request({
+        url: `http://www.omdbapi.com/?apikey=${apiKey}&t=${title}`,
+    }, (err, resp) => {
+        console.log(err);
+        if (err !== null) {
+            console.error(err);
+            return;
+        }
+        resolve(resp.body);
+    });
+});
+
+console.log(filmTitles);
+Promise.all(
+    filmTitles.titles.map(getMovieDetails)
+).then(v => {
+    fs.writeFileSync('bond-movie-data.json', JSON.stringify(v));
+});
+
