4d7126b0d234fb35b1a157872e8f514c2fdb9c43
John Byrne <troot1@gmail.com>
2019-11-04 23:01:54 +0000

    wip: add globbing and producer to watch review

--- watch-reviews/app/setup.js
+++ watch-reviews/app/setup.js
@@ -9,56 +9,47 @@ const watch = require('glob-watcher');
 const fg = require('fast-glob');
 
 // Raw chokidar instance
-const watcher = watch(['./**/*'], { ignoreInitial: false });
+const watcher = watch(['./storage/**/*'], { ignoreInitial: false });
 
-// Listen for the 'change' event to get `path`/`stat`
-// No async completion available because this is the raw chokidar instance
-watcher.on('change', function(path, stat) {
-  logger.info({ code: 'PRODUCER_FILE_CHANGE', data: { path, stat } });
-  // `path` is the path of the changed file
-  // `stat` is an `fs.Stat` object (not always available)
-});
-
-// Listen for other events
-// No async completion available because this is the raw chokidar instance
-watcher.on('add', function(path, stat) {
-  // `path` is the path of the changed file
-  // `stat` is an `fs.Stat` object (not always available)
-});
-
-logger.info('Starting up...');
-logger.info(`CACHE_ENV: ${process.env.CACHE_ENV}`);
-logger.info(`DIR: ${__dirname}`);
+logger.info({ code: 'WATCH_REVIEW_START', msg: `Starting up...\n\nCACHE_ENV: ${process.env.CACHE_ENV}\n\nDIR: ${__dirname}\n\nSettings: ${JSON.stringify(config.get())}` });
 
 config.file(`./config/config.json`);
 
 if (process.env.CACHE_ENV) {
   config.file(`./config/config.${process.env.CACHE_ENV}.json`);
 }
-logger.info(`Settings: ${JSON.stringify(config.get())}`);
 
+const client = new kafka.KafkaClient({
+  kafkaHost: config.get('app:kafka_url'),
+});
+
+const producer = new Producer(client);
 
 (async () => {
   const entries = await fg(['/opt/watch_reviews/storage/**/*']);
-  logger.info({ code: 'HERE', entries });
 
-  const client = new kafka.KafkaClient({
-    kafkaHost: config.get('app:kafka_url'),
-  });
-  const producer = new Producer(client);
-  const km = new KeyedMessage('key', 'message');
-  const payloads = [
-      { topic: 'BondMoviesToBeProcessed', messages: [km] }
-  ];
-  producer.on('ready', () => {
-    logger.info({ code: 'PRODUCER_READY' });
+  const postMovieToProcess = reviewFileName => {
+    const km = new KeyedMessage('reviewFileName', reviewFileName);
+    const payloads = [
+        { topic: 'BondMoviesToBeProcessed', messages: [km] }
+    ];
     producer.send(payloads, (err, data) => {
       if (err) {
-        logger.error({ code: 'PRODUCER_SEND', error: err });
-        process.exit(1);
+        logger.info({ code: 'PRODUCER_SEND_ERROR', err });
       }
       logger.info({ code: 'PRODUCER_SEND', data });
-      process.exit(0);
+    });
+  };
+
+  producer.on('ready', () => {
+    watcher.on('change', path => {
+      logger.info({ code: 'PRODUCER_FILE_CHANGE', data: { path } });
+      postMovieToProcess(path);
+    });
+
+    watcher.on('add', path => {
+      logger.info({ code: 'PRODUCER_FILE_ADD', data: { path } });
+      postMovieToProcess(path);
     });
   });
 
