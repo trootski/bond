bba9a8815c873f8ccf39dbf0dabe2e8d4e53aac9
Byrne, John <John.Byrne@hmhco.com>
2019-11-11 21:20:26 +0000

    chore: add dynamodb put to kafka consumer

--- process-queue/app/setup.js
+++ process-queue/app/setup.js
@@ -17,9 +17,6 @@ config.file(`./config/config.json`);
 const converter = new showdown.Converter();
 
 (async () => {
-  logger.info({
-    code: 'PROCESS_QUEUE_START',
-    msg: `Starting up...\n\nkafkaHost: ${config.get('kafka:url')}` });
   const client = new kafka.KafkaClient({
     kafkaHost: config.get('kafka:url'),
   });
@@ -29,65 +26,70 @@ const converter = new showdown.Converter();
   ], { autoCommit: false, fetchMaxWaitMs: 1000, fetchMaxBytes: 1024 * 1024 });
 
   consumer.on('message', async m => {
-    logger.info({ code: 'CONSUMER_MESSAGE', data: m });
     const fname = (m.value.match(/storage\/reviews\/(.+)$/) || [])[1];
     const stub = (m.value.match(/(.+)\/(.+)(\.md)$/) || [])[2];
+    if (!fname || !stub) return;
+    logger.info({ code: 'CONSUMER_MESSAGE_RECEIVED', fname, stub });
     try {
       const markdown = fs.readFileSync(`/opt/process_queue/storage/reviews/${fname}`, 'utf8');
       const html = converter.makeHtml(markdown);
       const filmEntry = filmData.data.find(v => v.review === fname);
-      logger.info({ code: 'CONSUMER_INFO', filmEntry });
 
       const client = redis.createClient({
         host: config.get('redis:url'),
         port: config.get('redis:port'),
       });
       const getAsync = promisify(client.get).bind(client);
-      const OMDBData = await getAsync(filmEntry.title);
+      const OMDBData = JSON.parse((await getAsync(filmEntry.title)));
 
-      const dataToPersist = {
-        ...filmEntry,
-        ...(JSON.parse(OMDBData)),
-        html,
-      };
-      logger.info({ code: 'CONSUMER_INFO', dataToPersist });
+      if (OMDBData) {
 
+        const dynamoDbParams = {
+          apiVersion: config.get('dynamodb:apiVersion'),
+          convertEmptyValues: true,
+          accessKeyId: config.get('dynamodb:accessKeyId'),
+          secretAccessKey: config.get('dynamodb:secretAccessKey'),
+          endpoint: config.get('dynamodb:dynamoDBEndpoint'),
+          logger,
+          region: config.get('dynamodb:region'),
+        };
 
-      const dynamoDbParams = {
-        apiVersion: config.get('dynamodb:apiVersion'),
-        convertEmptyValues: true,
-        accessKeyId: config.get('dynamodb:accessKeyId'),
-        secretAccessKey: config.get('dynamodb:secretAccessKey'),
-        endpoint: config.get('dynamodb:dynamoDBEndpoint'),
-        logger,
-        region: config.get('dynamodb:region'),
-      };
+        const client = await new AWS.DynamoDB(dynamoDbParams);
+        const dynamodb = await new AWS.DynamoDB.DocumentClient({
+          params: dynamoDbParams,
+          service: client,
+          convertEmptyValues: true,
+        });
 
-      logger.info('Connecting to DynamoDB...');
-      const dynamodb = await new AWS.DynamoDB(dynamoDbParams);
+        const checkItem = await dynamodb.get({
+          TableName: 'BondMovies',
+          Key: {
+            'Director': OMDBData.Director,
+            'MovieTitle': OMDBData.Title,
+          },
+        }).promise();
 
-      const checkItem = await dynamodb.getItem({
-        TableName: 'BondMovies',
-        Key: {
-          "Director": {
-            S: "Martin Campbell",
-           },
-          "MovieTitle": {
-            S: "Casino Royale",
-           }
-        },
-      }).promise();
-      logger.info({ code: 'CONSUMER_INFO', msg: `Results: ${JSON.stringify(checkItem)}` });
+        const dateCreated = (checkItem.Item) ? checkItem.Item.dateCreated : new Date().toISOString();
+        const dataToPersist = {
+          ...filmEntry,
+          ...OMDBData,
+          html,
+          ...{
+           "Director": OMDBData.Director,
+           "MovieTitle": OMDBData.Title,
+           dateCreated: dateCreated,
+          }
+        };
+        const createRecord = await dynamodb.put({
+          Item: dataToPersist,
+          ReturnConsumedCapacity: 'TOTAL',
+          TableName: 'BondMovies',
+        }).promise();
 
-      // logger.info({ code: 'CONSUMER_INFO', msg: `Setting new entry ${dataToPersist.title}` });
-      // const createRecord = await dynamodb.putItem({
-      //   TableName: 'BondMovies',
-      //   Item: dataToPersist,
-      // }).promise();
-
-      // logger.info({ code: 'CONSUMER_INFO', createRecord });
+        logger.info({ code: 'CONSUMER_INFO', createRecord, dataToPersist });
+      }
     } catch (e) {
-      logger.error({ code: 'CONSUMER_ERROR', error: e });
+      logger.error({ code: 'CONSUMER_MESSAGE_ERROR', error: e });
     }
   });
 
