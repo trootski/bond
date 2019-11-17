011b078f47f58a4f0097fec71772b44564381c68
John Byrne <troot1@gmail.com>
2019-11-17 21:18:13 +0000

    wip: clean up scan tables code

--- dynamodb_setup/app/scan-table.js
+++ dynamodb_setup/app/scan-table.js
@@ -1,38 +1,29 @@
 #!/usr/bin/env node
 
-const logger = require('pino')();
 const AWS = require('aws-sdk');
 const config = require('nconf');
+const logger = require('pino')().child({ app: 'DYNAMODB_SETUP' });
+const { getDynamoDBClient } = require('./utils/dynamo.js');
 
-config.file('./config.json');
+config.file('/opt/dynamodb_setup/config/config.json');
 
-logger.info({ code: 'DYNAMO_SCAN_START', msg: `Connecting to DynamoDB
-Settings: ${JSON.stringify(config.get())}` });
+logger.info({
+  msg: `Connecting to DynamoDB\n\nSettings: ${JSON.stringify(config.get())}`,
+});
 
 (async () => {
   try {
-    const dynamoDbParams = {
-      apiVersion: config.get('dynamodb:apiVersion'),
-      convertEmptyValues: true,
-      accessKeyId: config.get('dynamodb:accessKeyId'),
-      secretAccessKey: config.get('dynamodb:secretAccessKey'),
-      endpoint: config.get('dynamodb:dynamoDBEndpoint'),
-      logger,
-      region: config.get('dynamodb:region'),
-    };
+    const dynamodb = await getDynamoDBClient({ config, logger, waitForTable: false });
 
-    logger.info({ code: 'DYNAMO_SCAN_INFO', msg: 'Connecting to DynamoDB...' });
-    const dynamodb = await new AWS.DynamoDB(dynamoDbParams);
-
-    logger.info({ code: 'DYNAMO_SCAN_INFO', msg: 'Scan all rows in table...' });
+    logger.info({ msg: 'Scan all rows in table...' });
     const allResults = await dynamodb.scan({
       TableName: 'BondMovies'
     }).promise();
 
-    logger.info(allResults);
+    logger.info({ msg: allResults });
 
     process.exit(0);
-  } catch (e) {
-    logger.error({ code: 'DYNAMO_SCAN_ERROR', err });
+  } catch (err) {
+    logger.error({ err });
   }
 })();
