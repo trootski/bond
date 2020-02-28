478ca9bc29b67b2f8277f2b051f268313729b115
John Byrne <john.byrne@hmhco.com>
2020-02-28 14:24:31 +0000

    test: add dbunit scenarios to integration tests

--- bond-movies-api-spring-postgresql/src/test/java/org/troot/bondmoviesapispringpostgresql/controller/BondMovieControllerTestIT.java
+++ bond-movies-api-spring-postgresql/src/test/java/org/troot/bondmoviesapispringpostgresql/controller/BondMovieControllerTestIT.java
@@ -1,23 +1,32 @@
 package org.troot.bondmoviesapispringpostgresql.controller;
 
+import com.github.springtestdbunit.TransactionDbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
+@TestExecutionListeners({
+        DependencyInjectionTestExecutionListener.class,
+        TransactionDbUnitTestExecutionListener.class
+})
 public class BondMovieControllerTestIT {
 
     @Autowired
@@ -27,11 +36,20 @@ public class BondMovieControllerTestIT {
     private MockMvc mockMvc;
 
     @Test
-    public void testGetAllMovies() throws Exception{
+    @DatabaseSetup("classpath:0-movies.xml")
+    public void testGetAllMovies_0_Movies() throws Exception{
         MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/v1/bond-movies"))
                 .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(0)))
                 .andReturn();
-        String resultDOW = result.getResponse().getContentAsString();
-        assertEquals(resultDOW, "[]");
+    }
+
+    @Test
+    @DatabaseSetup("classpath:5-movies.xml")
+    public void testGetAllMovies_5_Movies() throws Exception{
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/v1/bond-movies"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(5)))
+                .andReturn();
     }
 }
