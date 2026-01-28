b1f1ac4cbb1dc8b08f44892204806c33ce0ce55f
trootski <troot1@gmail.com>
2026-01-28 22:34:01 +0000

    chore: upgrade bond movies api spring dynamo

--- bond-movies-api-spring-dynamodb/src/main/java/org/troot/BondSpring/rest/controller/BondMovieController.java
+++ bond-movies-api-spring-dynamodb/src/main/java/org/troot/BondSpring/rest/controller/BondMovieController.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,10 +17,13 @@ import java.util.List;
 @RestController
 public class BondMovieController {
 
-  @Autowired
-  BondMovieService bondMovieService;
+  private final BondMovieService bondMovieService;
 
   private static final Logger logger = LoggerFactory.getLogger(BondMovieController.class);
+
+  public BondMovieController(BondMovieService bondMovieService) {
+    this.bondMovieService = bondMovieService;
+  }
 
   @RequestMapping(value = "/v1/bond-movies", method = RequestMethod.GET)
   public List<BondMovie> getAllMovies() {
@@ -32,14 +34,9 @@ public class BondMovieController {
   @RequestMapping(value = "/v1/bond-movies/{title:.+}", method = RequestMethod.GET)
   public BondMovie getBondMovie(@PathVariable(value = "title", required = true) String title) {
     logger.info("GET /v1/bond-movies/{}", title);
-    List<BondMovie> bondMovies = bondMovieService.getBondMovie(title);
-    if (bondMovies.isEmpty() || bondMovies.get(0) == null) {
-      throw new ResponseStatusException(
-        HttpStatus.NOT_FOUND, "bond movie not found"
-      );
-    } else {
-      return bondMovies.get(0);
-    }
+    return bondMovieService.getBondMovie(title)
+        .orElseThrow(() -> new ResponseStatusException(
+            HttpStatus.NOT_FOUND, "bond movie not found"));
   }
 
   @RequestMapping(value = "/v1/bond-movies/{title:.+}", method = RequestMethod.PUT)
--- bond-movies-api-spring-dynamodb/src/main/java/org/troot/BondSpring/service/BondMovieService.java
+++ bond-movies-api-spring-dynamodb/src/main/java/org/troot/BondSpring/service/BondMovieService.java
@@ -1,24 +1,30 @@
 package org.troot.BondSpring.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.troot.BondSpring.entity.BondMovie;
 import org.troot.BondSpring.repository.BondMovieRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class BondMovieService {
-  @Autowired
-  BondMovieRepository bondMovieRepository;
+
+  private final BondMovieRepository bondMovieRepository;
+
+  public BondMovieService(BondMovieRepository bondMovieRepository) {
+    this.bondMovieRepository = bondMovieRepository;
+  }
 
   public List<BondMovie> getAllMovies() {
     return bondMovieRepository.findByMovieType("movie");
   }
 
-  public List<BondMovie> getBondMovie(String title) {
+  public Optional<BondMovie> getBondMovie(String title) {
     return bondMovieRepository.findByTitle(title);
   }
 
-  public BondMovie createBondMovie(BondMovie bondMovie) { return bondMovieRepository.save(bondMovie); }
+  public BondMovie createBondMovie(BondMovie bondMovie) {
+    return bondMovieRepository.save(bondMovie);
+  }
 }
