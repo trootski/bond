81574c0cb3c55878811e55128ef30d45f4da7b15
trootski <troot1@gmail.com>
2026-02-10 22:05:17 +0000

    chore: remove kafka node

--- process-queue-spring-camel-kafka/src/main/java/org/troot/bondmovieapi/config/CamelConfig.java
+++ process-queue-spring-camel-kafka/src/main/java/org/troot/bondmovieapi/config/CamelConfig.java
@@ -1,33 +1,15 @@
 package org.troot.bondmovieapi.config;
 
 import org.apache.camel.CamelContext;
-import org.apache.camel.spring.boot.CamelContextConfiguration;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.apache.camel.FluentProducerTemplate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.troot.bondmovieapi.route.BondMovieRouteBuilder;
 
 @Configuration
 public class CamelConfig {
 
-    @Autowired
-    private BondMovieRouteBuilder bondMovieRouteBuilder;
-
     @Bean
-    CamelContextConfiguration contextConfiguration() {
-        return new CamelContextConfiguration() {
-            @Override
-            public void beforeApplicationStart(CamelContext context) {
-                try {
-                    context.addRoutes(bondMovieRouteBuilder);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-            @Override
-            public void afterApplicationStart(CamelContext camelContext) {
-
-            }
-        };
+    public FluentProducerTemplate fluentProducerTemplate(CamelContext camelContext) {
+        return camelContext.createFluentProducerTemplate();
     }
 }
